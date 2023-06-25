d870f59569971cd8dd467ed6bc9cdf39b182b712
abostef747 <s.abostef@yahoo.com>
2023-06-25 11:44:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -29,3 +29,43 @@ def homepage():
 @app.route('/hello', methods=['GET'])
 def hello():
     return "Hello"
+
+from flask import Flask, request, jsonify
+from moviepy.editor import VideoFileClip
+import requests
+from urllib.parse import urlparse
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, World!"
+
+@app.route('/video_length', methods=['POST'])
+def video_length():
+    data = request.json
+    video_url = data['url']
+    
+    # Parse the url to get the video name
+    a = urlparse(video_url)
+    video_name = os.path.basename(a.path)
+
+    # Download the video file
+    with requests.get(video_url, stream=True) as r:
+        r.raise_for_status()
+        with open(video_name, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+
+    # Load video file and get its duration
+    clip = VideoFileClip(video_name)
+    duration = clip.duration
+
+    # Cleanup the downloaded video file
+    os.remove(video_name)
+
+    return jsonify({'length': duration})
+
+if __name__ == '__main__':
+    app.run(debug=True)
